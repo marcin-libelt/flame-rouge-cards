f7f7482894f5412119050464f7da23e50257d3c9
Marcin Libelt <marcin.libelt@gmail.com>
2024-06-11 23:17:12 +0200

    installed Radix-ui framework

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "@radix-ui/themes/styles.css";
 import RiderCardsContextProvider from "./context/RiderCardsContextProvider.tsx";
+import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RiderCardsContextProvider>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </RiderCardsContextProvider>
   </React.StrictMode>
 );
